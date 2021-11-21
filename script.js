2d557200f8076bc5c3b8214c304d06a991980db4
Maksim <maksimukoloff@yandex.ru>
2021-11-21 17:03:17 +0300

    Moving Zeros To The End

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
+//Simple Pig Latin_________________________________________________
+function pigIt(str){
+    let arrStr = str.split(' ')
+    arrStr.forEach(el => {
+        
+    });
+   return pigIt('Pig latin is cool');
+  }
+  console.log(pigIt('Pig latin is cool'));
+
+//Decathlon World Championship
+// let hundred 
+
+
+
+
+ //Who likes it?__________________________________________________
+//  function likes(names) {
+//     if (names.length === 0) {
+//       return 'no one likes this';
+//     }
+//     else if (names.length === 1) 
+//     {
+//       return ${names[0]} likes this;
+//     }
+//     else if ((names.length > 0) && (names.length<3))
+//     {
+//       return ${names[0]} and ${names[1]} like this;
+//     }
+//     else if ((names.length > 0) && (names.length === 3)) {
+//       return  ${names[0]}, ${names[1]} and ${names[2]} like this;
+//     } 
+//     else if ((names.length > 0) && (names.length === 4)) {
+//         return ${names[0]}, ${names[1]} and 2 others like this;
+//     }
+//   } 
+
+
 //@ts-check
 /**
  * 
- * @param {number} number 
- */
-function solution(number){
+ * @param {number} number*/
 
-  let digits = number.toString().split('').map((digits)=> +digits).slice(-4)//map перобразование в массив.slice берёт только последние цифры введённого числа
-  console.log(digits);
-}
-solution(1000)
+//Roman Numerals Encoder__________________________________________
+
+// function solution(number){
+//     const ones = number => [ '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'][number]
+//     const desc = number => [ '', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'][number]
+//     const hunds = number => [ '', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'][number]
+//     const mils = number => [ '', 'M', 'MM', 'MMM'][number]
+      
+//    let num = number.toString()
+//    while(num.length < 4) num = "0" + num; //цикл добавляет нули слева   
+//    let digits = num.split('').map((digits)=> +digits).slice(-4)//map перобразование в массив.slice берёт только последние цифры введённого числа
+//    return mils(digits[0]) + hunds(digits[1]) + desc(digits[2]) + ones(digits[3])
+// }
+// console.log (solution(11));
+// console.log (solution(22));
+// console.log (solution(1990));
+// console.log (solution(2007));
+
+
+
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //  function filter (callback) {
 //    let arr = this
